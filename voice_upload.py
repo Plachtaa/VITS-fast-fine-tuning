c9b8a0a44686600912de06cc7b911aaae31954a6
Plachta <liusongting07@gmail.com>
2023-02-16 16:39:34 +0800

    upload files

--- voice_upload.py
+++ voice_upload.py
@@ -8,4 +8,4 @@ if not os.path.exists(upload_path):
   os.mkdir(upload_path)
 for filename in uploaded.keys():
     #将上传的文件移动到指定的位置上
-    shutil.move(os.path.join(basepath, filename), os.path.join(upload_path, filename))
+    shutil.move(os.path.join(basepath, filename), os.path.join(upload_path, "custom_character_voice.zip"))
