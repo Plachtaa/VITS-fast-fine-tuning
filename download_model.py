a1b6eb54e49bf68725c1bf9cafe6877ff2adce72
Plachta <liusongting07@gmail.com>
2023-02-16 18:29:06 +0800

    upload files

--- download_model.py
+++ download_model.py
@@ -1,3 +1,3 @@
 from google.colab import files
 files.download("./OUTPUT_MODEL/G_latest.pth")
-files.download("./configs/finetune_speaker.json")
+files.download("./configs/modified_finetune_speaker.json")
