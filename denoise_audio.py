4c45e6a74cd5982a6f226165cc01046ec58680c0
Evilmass <evil0mass@gmail.com>
2023-03-05 14:55:32 +0800

    multithreading event

--- denoise_audio.py
+++ denoise_audio.py
@@ -1,18 +1,31 @@
 import os
+from concurrent.futures import ProcessPoolExecutor
+
 import torchaudio
+
+WORKERS = 3  # 6G显存+16G内存，请根据机器配置调整
 raw_audio_dir = "./raw_audio/"
 denoise_audio_dir = "./denoised_audio/"
 filelist = list(os.walk(raw_audio_dir))[0][2]
 
-for file in filelist:
+
+def denoise_audio(file):
     if file.endswith(".wav"):
         os.system(f"demucs --two-stems=vocals {raw_audio_dir}{file}")
-for file in filelist:
+
     file = file.replace(".wav", "")
-    wav, sr = torchaudio.load(f"./separated/htdemucs/{file}/vocals.wav", frame_offset=0, num_frames=-1, normalize=True,
+    wav, sr = torchaudio.load(f"./separated/htdemucs/{file}/vocals.wav",
+                              frame_offset=0,
+                              num_frames=-1,
+                              normalize=True,
                               channels_first=True)
     # merge two channels into one
     wav = wav.mean(dim=0).unsqueeze(0)
     if sr != 22050:
         wav = torchaudio.transforms.Resample(orig_freq=sr, new_freq=22050)(wav)
     torchaudio.save(denoise_audio_dir + file + ".wav", wav, 22050, channels_first=True)
+
+
+if __name__ == "__main__":
+    with ProcessPoolExecutor(max_workers=WORKERS) as executor:
+        executor.map(denoise_audio, filelist)
--- video2audio.py
+++ video2audio.py
@@ -1,10 +1,27 @@
-from moviepy.editor import AudioFileClip
 import os
+from concurrent.futures import ThreadPoolExecutor
+
+from moviepy.editor import AudioFileClip
+
 video_dir = "./video_data/"
 audio_dir = "./raw_audio/"
 filelist = list(os.walk(video_dir))[0][2]
-for file in filelist:
-    if file.endswith(".mp4"):
-        my_audio_clip = AudioFileClip(video_dir + file)
-        my_audio_clip.write_audiofile(audio_dir + file.rstrip(".mp4") + ".wav")
 
+
+def generate_infos():
+    videos = []
+    for file in filelist:
+        if file.endswith(".mp4"):
+            videos.append(file)
+    return videos
+
+
+def clip_file(file):
+    my_audio_clip = AudioFileClip(video_dir + file)
+    my_audio_clip.write_audiofile(audio_dir + file.rstrip(".mp4") + ".wav")
+
+
+if __name__ == "__main__":
+    infos = generate_infos()
+    with ThreadPoolExecutor(max_workers=os.cpu_count() - 2) as executor:
+        executor.map(clip_file, infos)
