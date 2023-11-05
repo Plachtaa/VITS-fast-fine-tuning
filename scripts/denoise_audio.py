5bfe33d0a890cb57d2ed0d7fa53d8230b1e865c8
xxnuo <54252779+xxnuo@users.noreply.github.com>
2023-11-05 11:19:26 +0800

    Optimization: Added multithreading support for denoising_audio.py.
    Optimization: Added multithreading support for denoising multiple small wav files. 
    
    Reason: The previous method of processing multiple files was too slow. Now, we are using multithreading to handle scenarios with many small files efficiently.

--- scripts/denoise_audio.py
+++ scripts/denoise_audio.py
@@ -1,6 +1,8 @@
 import os
 import json
 import torchaudio
+import concurrent.futures
+
 raw_audio_dir = "./raw_audio/"
 denoise_audio_dir = "./denoised_audio/"
 filelist = list(os.walk(raw_audio_dir))[0][2]
@@ -8,15 +10,39 @@ filelist = list(os.walk(raw_audio_dir))[0][2]
 with open("./configs/finetune_speaker.json", 'r', encoding='utf-8') as f:
     hps = json.load(f)
 target_sr = hps['data']['sampling_rate']
-for file in filelist:
-    if file.endswith(".wav"):
-        os.system(f"demucs --two-stems=vocals {raw_audio_dir}{file}")
-for file in filelist:
-    file = file.replace(".wav", "")
-    wav, sr = torchaudio.load(f"./separated/htdemucs/{file}/vocals.wav", frame_offset=0, num_frames=-1, normalize=True,
+
+def get_filename(path):
+    if os.path.isdir(path):
+        print(f"isDir,return: {path}")
+        return ""
+    else:
+        file_name = os.path.basename(path)
+        file_name = os.path.splitext(file_name)[0]
+        return file_name
+
+def process_file(file):
+    file_name = get_filename(file)
+    if file_name == "":
+        return
+    if not os.path.exists(f"./separated/htdemucs/{file_name}/vocals.wav"):
+        command = f"demucs --two-stems=vocals {raw_audio_dir}{file}"
+        print(f"Process: {command}")
+        os.system(command)
+        print(f"Seperated: {file}")
+    wav, sr = torchaudio.load(f"./separated/htdemucs/{file_name}/vocals.wav", frame_offset=0, num_frames=-1, normalize=True,
                               channels_first=True)
     # merge two channels into one
     wav = wav.mean(dim=0).unsqueeze(0)
     if sr != target_sr:
         wav = torchaudio.transforms.Resample(orig_freq=sr, new_freq=target_sr)(wav)
-    torchaudio.save(denoise_audio_dir + file + ".wav", wav, target_sr, channels_first=True)
+    torchaudio.save(denoise_audio_dir + file, wav, target_sr, channels_first=True)
+    print(f"Ok: {file}")
+
+num_threads = 16
+with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+    # Submit each file to the executor for processing
+    futures = []
+    for file in filelist:
+        futures.append(executor.submit(process_file, file))
+    # Wait for all tasks to complete
+    concurrent.futures.wait(futures)
